94c57a0cbe41614b2d8cd7647e8de51fea1fd9c2
Spencer King <spencerking@users.noreply.github.com>
2020-01-16 09:07:17 -0600

    Load Qiskit with PyCall, implement version wrappers

--- src/Qiskit.jl
+++ src/Qiskit.jl
@@ -1,5 +1,19 @@
 module Qiskit
 
-greet() = print("Hello World!")
+using PyCall
+
+const qiskit = PyNULL()
+
+function init()
+    try
+	copy!(qiskit, pyimport("qiskit"))
+    catch err
+        error("The Python Qiskit package could not be imported. You must install Python Qiskit before using this package. The error was $err")
+    end
+end
+
+version() = qiskit.__version__
+
+qiskit_version() = qiskit.__qiskit_version__
 
 end # module
