3dae5fd6ca64ce494b39fe92ea58acace3eac92a
Spencer King <spencerking@users.noreply.github.com>
2020-01-16 16:42:23 -0600

    Example 1 > code cells finished up until the IBM Q section

--- src/Qiskit.jl
+++ src/Qiskit.jl
@@ -44,6 +44,10 @@ function plot_state_city(state)
     qiskit.visualization.plot_state_city(state)
 end
 
+function plot_histogram(data)
+    qiskit.visualization.plot_histogram(data)
+end
+
 
 # Version
 version() = qiskit.__version__
