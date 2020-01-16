ac45f4db041ca1717ca74634c3f00b097c7d46d3
Spencer King <spencerking@users.noreply.github.com>
2020-01-16 16:33:14 -0600

    Progress on example 1 getting started

--- src/Qiskit.jl
+++ src/Qiskit.jl
@@ -12,8 +12,41 @@ function init()
     end
 end
 
-version() = qiskit.__version__
+function QuantumCircuit(x)
+    qiskit.QuantumCircuit(x)
+end
 
+function QuantumCircuit(x, y)
+    qiskit.QuantumCircuit(x, y)
+end
+
+function execute(circuit, backend)
+    qiskit.execute(circuit, backend)
+end
+
+function execute(circuit, backend, shots)
+    qiskit.execute(circuit, backend, shots=1024)
+end
+
+# Aer
+function get_backend(b)
+    qiskit.Aer.get_backend(b)
+end
+
+#module Aer
+#    function get_backend(b)
+#	qiskit.aer.get_backend(b)
+#    end
+#end
+
+# Visualization
+function plot_state_city(state)
+    qiskit.visualization.plot_state_city(state)
+end
+
+
+# Version
+version() = qiskit.__version__
 qiskit_version() = qiskit.__qiskit_version__
 
 end # module
